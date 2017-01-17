05b5736416c3d6bb82e870943eb78ef70911d29d
fgn <fgndevelop@posteo.org>
2017-01-17 20:23:17 +0100

    Corrected a Makefile typo

--- framework/header/05_help.sh
+++ framework/header/05_help.sh
@@ -4,9 +4,13 @@
 #
 # "--help" and "--version / -V" are expected to work for all scripts. Period.
 # Since the getopts() library is not a requirement, we do not rely on it's
-# functionality and simple parse the command line ourselves.
+# functionality and simply parse the command line ourselves.
 # Since these functions are either called during script initialization or 
 # not at all, they will be unset by the init code. 
+#
+# We do not automagically output the --help / --version information when 
+# printing usage text as most of the times text formatting won't match
+# and would then require editing the header file, too.
 
 _sfw_help_version_check()
 {
@@ -22,10 +26,14 @@ Usage: $_sfw_script_name $_sfw_synopsis
 @SCRIPT_SHORT_DESCRIPTION@
 
 _usage_text_replaced_by_sed_
-
-For more details see ${_sfw_man_title}(${_sfw_man_section}).
 EOF_USAGE_TEXT
-	exit 0
+
+        # If a man page has been defined, inform the user about it
+        if [ -n "${_sfw_man_title}" -a -n "${_sfw_man_section}" ]; then
+          echo 
+          echo "For detailed documentation see ${_sfw_man_title}(${_sfw_man_section})"
+        fi
+        exit 0
 	;;
 
       # Print version information
